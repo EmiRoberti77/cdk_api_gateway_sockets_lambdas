1db162b74998a6d73248734c07deaaffd04127d3
EmiRoberti77 <emi@datamotion.ai>
2024-08-29 06:12:20 +0100

    updated test

--- test/sendBroadcast.test.ts
+++ test/sendBroadcast.test.ts
@@ -7,8 +7,22 @@ interface Message {
 }
 
 const body: Message = {
-  connectionId: "dNRWvd4OIAMCL6g=",
-  message: "testLambda",
+  connectionId: "dQUvSfHKoAMCF1Q=",
+  message: `{
+ "variable": "PN ASenna",
+  "value": "aberto",
+ "location": {
+ "type": "Point",
+  "coordinates": [ -48.521309, -25.506428  ]
+ },
+ "metadata": {
+  "color": "Black",
+ "icon": "train",
+  "x": 0.500000,
+  "y": 0.500000,
+ "triggered": "2024-07-18T13:56:07.785Z"
+ }
+ }`,
   endpoint: `https://369eizuvs5.execute-api.us-east-1.amazonaws.com/dev`,
 };
 
